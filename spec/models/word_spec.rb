819f521a145ca1cae870f61e9c5b31c996f1224a
Natalie McCroy & Xander Glassman <pair+nataliemac81.xanderglassman@devbootcamp.com>
2014-07-17 10:07:58 -0500

    nice gemfile, bro that seems beter. --Travis

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Word do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @word = Word.new(body: "hi", syllable_count: 1)
+  end
+
+  subject { @word }
+
+  it { should respond_to(:body) }
+  it { should respond_to(:syllable_count) }
+  it { should be_valid }
 end
