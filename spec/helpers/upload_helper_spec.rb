61077397bdc9dda49c7a14b37db8be7289f7a6f6
Natalie McCroy & Xander Glassman <pair+nataliemac81.xanderglassman@devbootcamp.com>
2014-07-17 19:16:47 -0500

    reworked model tests pt 2

--- spec/helpers/upload_helper_spec.rb
+++ spec/helpers/upload_helper_spec.rb
@@ -10,6 +10,6 @@ require 'spec_helper'
 #     end
 #   end
 # end
-describe UploadHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
+# describe UploadHelper do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
